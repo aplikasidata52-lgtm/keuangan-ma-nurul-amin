b6d5ea7f0d26089ce6cbe6acaeadc642f7e95066
aplikasidata52-lgtm <aplikasidata52@gmail.com>
2026-07-27 14:45:51 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+npm create vite@latest aplikasi-keuangan -- --template react
+cd aplikasi-keuangan
+npm install lucide-react tailwindcss @tailwindcss/vite
+2. Pasang kode di atas ke dalam file `src/App.jsx`.
